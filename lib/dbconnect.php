e38a3acc3e3b7bf19bb2aed91c28fc8f37d077ff
Dimitrios Kyriakidis <93831628+kyriakidisdimitrios@users.noreply.github.com>
2022-10-26 16:13:23 +0300

    Create dbconnect.php

---
+++ lib/dbconnect.php
@@ -0,0 +1,21 @@
+<?php
+$host='localhost';
+$db = 'bluff';
+require_once "db_upass.php";
+
+$user=$DB_USER;
+$pass=$DB_PASS;
+
+
+if(gethostname()=='users.iee.ihu.gr') {
+	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2003/it032378/mysql/run/mysql.sock');
+} else {
+        //test
+		$pass=null;
+        $mysqli = new mysqli($host, $user, $pass, $db);
+}
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . 
+    $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}?>
